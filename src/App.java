63361985d104aa187e918f2c69a1e1e1ff8db505
unknown <pedroandres.duque@gmail.com>
2025-08-12 21:49:35 -0500

    ahora si va el codigo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Programa para realizar calculos de la Ecuación Lineal");
+
+        // declarar objeto para leer datos por consola
+        Scanner lector = new Scanner(System.in);
+
+        // leer los datos de entrada
+        System.out.println("Coordenadas del primer punto");
+        System.out.print("x ?");
+        double x1 = lector.nextDouble();
+        System.out.print("y ?");
+        double y1 = lector.nextDouble();
+
+        System.out.println("Coordenadas del segundo punto");
+        System.out.print("x ?");
+        double x2 = lector.nextDouble();
+        System.out.print("y ?");
+        double y2 = lector.nextDouble();
+
+        // proceso
+        double distancia = Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+
+        // salida
+        System.out.println("La distancia entre los puntos es " + distancia);
+
     }
 }
